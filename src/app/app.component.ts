af5d043f5be96e61264292db51ff392497490b96
Atacan Çelikkol <atacan@ovidos.com>
2019-04-21 23:43:46 +0300

    Hello World! :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'OS-Firebase-Chat';
+
+  constructor(db: AngularFireDatabase) {
+    db.list('messages').valueChanges().subscribe(x => console.log(x));
+  }
 }
