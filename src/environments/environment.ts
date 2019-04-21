03912074e9adc36e2bf719b1e8b0ef375723295d
Atacan Çelikkol <atacan@ovidos.com>
2019-04-21 21:44:07 +0300

    e

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyALqdpKv2uwEZUy2_vkkcp9SyFfdQrLecg',
-    authDomain: 'os-firebase-chat.firebaseapp.com',
-    databaseURL: 'https://os-firebase-chat.firebaseio.com',
-    projectId: 'os-firebase-chat',
-    storageBucket: 'os-firebase-chat.appspot.com',
-    messagingSenderId: '1009739325204'
+    // Your Firebase configuration should be here
   }
 };
